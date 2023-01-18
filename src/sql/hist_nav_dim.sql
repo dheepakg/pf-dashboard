b4c760222de54d5b1f2a16860701421d8876c3db
dheepakg <sslvgdek@gmail.com>
2023-01-18 14:32:42 +0530

    added sql file

---
+++ src/sql/hist_nav_dim.sql
@@ -0,0 +1,12 @@
+CREATE TABLE hist_nav_dim(
+    id integer primary key AUTOINCREMENT,
+    nav_on text,
+    ppfas NUMERIC,
+    mirae NUMERIC,
+    nifty50 NUMERIC,
+    icici_tax NUMERIC,
+    icici_scap NUMERIC,
+    prima NUMERIC,
+    absl96 NUMERIC,
+    insert_dttm text
+    );
