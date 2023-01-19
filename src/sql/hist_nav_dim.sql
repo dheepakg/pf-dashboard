4a8d702ad7335cc207a9ad9fd583ba99f4cc14e5
dheepakg <sslvgdek@gmail.com>
2023-01-19 12:59:33 +0530

    Column names aligned with pandas columns

--- src/sql/hist_nav_dim.sql
+++ src/sql/hist_nav_dim.sql
@@ -1,12 +1,11 @@
 CREATE TABLE hist_nav_dim(
     id integer primary key AUTOINCREMENT,
-    nav_on text,
+    date text,
     ppfas NUMERIC,
     mirae NUMERIC,
     nifty50 NUMERIC,
     icici_tax NUMERIC,
     icici_scap NUMERIC,
     prima NUMERIC,
-    absl96 NUMERIC,
-    insert_dttm text
+    absl96 NUMERIC
     );
