db79676ede8bb6b6253c0f9435ad029f32b7c5f5
dheepakg <sslvgdek@gmail.com>
2023-02-06 17:18:01 +0530

    Txn status check

--- src/add_transactions.py
+++ src/add_transactions.py
@@ -3,15 +3,18 @@ import sqlite3
 
 
 txn_date = ['2022-06-03', '2022-06-17','2022-08-02','2022-09-02','2022-09-21',
-            '2022-10-04','2022-11-02','2022-12-02','2023-01-03','2023-01-27','2023-02-03']
+            '2022-10-04','2022-11-02','2022-12-02','2023-01-03','2023-01-27','2023-02-02']
 
 top_up = [False,True,False,False,True,False,False,False,False,True,False]
 price_paid = [5000,10000,5000,5000,5000,5000,5000,5000,5000,5000,5000]
 
 number_of_transaction = {len(txn_date), len(top_up), len(price_paid)}
 
+# A Transaction is complete if it has txn_date, top_up, price_paid;
+# If any of the above value is missing, Exit with exception
 if len(number_of_transaction) > 1:
-    raise 'Transaction details is incomplete. Check entries'
+    exit('Transaction details is incomplete. Check entries')
+
 
 df_ppfas = pd.DataFrame(data={
     'fund_name':['ppfas']*len(txn_date),
