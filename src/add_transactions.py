43f0758e18746cfa774b77dcf9e6dbf2daad1706
dheepakg <sslvgdek@gmail.com>
2023-02-06 17:14:42 +0530

    Param - value inserts

--- src/add_transactions.py
+++ src/add_transactions.py
@@ -1,16 +1,30 @@
 import pandas as pd
 import sqlite3
 
-df = pd.DataFrame(data={
-    'fund_name':['ppfas','ppfas','ppfas','ppfas','ppfas','ppfas','ppfas','ppfas','ppfas',],
-    'txn_date':['2022-06-03', '2022-06-17','2022-08-02','2022-09-02','2022-09-21','2022-10-04','2022-11-02','2022-12-02','2023-01-03'],
-    'top_up':[False,True,False,False,True,False,False,False,False],
-    'purchase_price':[5000,10000,5000,5000,5000,5000,5000,5000,5000],
-    'allotted_units':[101.37, 220.09, 98.72, 97.72, 96.17, 98.86, 96.7, 94.55, 97.6]
-}, index=range(9))
 
-print(df)
+txn_date = ['2022-06-03', '2022-06-17','2022-08-02','2022-09-02','2022-09-21',
+            '2022-10-04','2022-11-02','2022-12-02','2023-01-03','2023-01-27','2023-02-03']
+
+top_up = [False,True,False,False,True,False,False,False,False,True,False]
+price_paid = [5000,10000,5000,5000,5000,5000,5000,5000,5000,5000,5000]
+
+number_of_transaction = {len(txn_date), len(top_up), len(price_paid)}
+
+if len(number_of_transaction) > 1:
+    raise 'Transaction details is incomplete. Check entries'
+
+df_ppfas = pd.DataFrame(data={
+    'fund_name':['ppfas']*len(txn_date),
+    'txn_date': txn_date,
+    'top_up': top_up,
+    'purchase_price': price_paid,
+}, index=range(len(txn_date)))
+
+
+df_ppfas["txn_date"] = pd.to_datetime(df_ppfas["txn_date"])
+
+df_final = df_ppfas
 
 conn = sqlite3.connect('data/mutual-fund.db')
 
-df.to_sql('txn_dim', conn, if_exists='replace',index=True)
+df_final.to_sql('txn_dim', conn, if_exists='replace',index=True)
